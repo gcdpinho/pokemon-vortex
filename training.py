ca8230486d733ab2a9ccf8488e59dce20c6140f8
Gustavo Pinho <gcdpinho@gmail.com>
2017-12-12 02:43:20 -0200

    pot + ajustes

--- training.py
+++ training.py
@@ -39,7 +39,7 @@ def main():
     driver = webdriver.PhantomJS()
     leaders = [("BUG","415355"), ("DARK", "840815"), ("DRAGON", "415125"), ("FIRE", "19935"), ("FIRE", "381236"),
                 ("FLYING", "42587"), ("FLYING", "415267"), ("FLYING", "404027"), ("GHOST", "101386"), ("ELETRIC", "381236"),
-                ("ELETRIC", "301042"), ("FAIRY", "414997"), ("FAIRY", "413288"), ("FIGHINT", "544881"), ("FIGHINT", "889435"),
+                ("ELETRIC", "301042"), ("FAIRY", "414997"), ("FAIRY", "413288"), ("FIGHTING", "544881"), ("FIGHTING", "889435"),
                 ("GRASS", "502380"), ("GRASS", "381258"), ("GROUND", "112251"), ("GROUND", "413963"), ("ICE", "381236"),
                 ("NORMAL", "140845"), ("NORMAL", "70794"), ("POISON", "405797"), ("POISON", "416906"), ("PSYCHIC", "280993"),
                 ("PSYCHIC", "498104"), ("ROCK", "381236"), ("STEEL", "133733"), ("STEEL", "414302"), ("WATER", "396804"),
@@ -54,6 +54,7 @@ def main():
 
 def training(driver, url, leaders, iteration):
     ###### TRAINING ######
+    sleep = 3
     url = driver.current_url.replace("dashboard/", "")
     repeat = False
     while True:
@@ -74,7 +75,7 @@ def training(driver, url, leaders, iteration):
         choice = random.randint(0, len(natureLeaders)-1)
         driver.get(url + "battle-user/" + natureLeaders[choice][-1])
         # seleciona o ataque do pokemon
-        att = selectAttack(driver, url)
+        att = selectAttack(driver, url, sleep)
         # start Battle
         print("start Battle!")
         flag = True
@@ -84,23 +85,16 @@ def training(driver, url, leaders, iteration):
             nextPage = driver.find_elements_by_class_name("button-small")
             for element in nextPage:
                 # continue ou atack
-                if "select_attack" in element.get_attribute("value") or "attack" in element.get_attribute("value"):
-                    if first and "select_attack" not in element.get_attribute("value") and "attack" in element.get_attribute("value"):
-                        driver.find_element_by_class_name("attackForm").find_elements_by_tag_name("td")[att-1].find_element_by_tag_name("label").click()
-                        time.sleep(2)
-                        first = False
-                    print ("Continue" if "select_attack" in element.get_attribute('value') else "Attack")
-                    element.submit()
-                    time.sleep(2)
+                value = element.get_attribute("value")
+                if "select_attack" in value or "attack" in value:
+                    first = battleRound(driver, element, att, first, 50, sleep)
                     flag = True
-                    break
+                    break;
             if not flag:
                 try:
                     # continue - morte de um pokemon
-                    nextPage = driver.find_element_by_css_selector("input[value='pokechu']")
                     print ("Defeat")
-                    nextPage.submit()
-                    time.sleep(2)
+                    onClick(driver.find_element_by_css_selector("input[value='pokechu']"), 1, sleep)
                     flag = True
                 except:
                     # end battle
@@ -112,9 +106,7 @@ def training(driver, url, leaders, iteration):
                         decision = input("Rebattle Opponent (s/n): ").lower()
                     else:
                         # verifica o level do pokemon
-                        nextPage = driver.find_elements_by_class_name("menu-tab")[1]
-                        nextPage.click()
-                        time.sleep(2)
+                        onClick(driver.find_elements_by_class_name("menu-tab")[1], 0, sleep)
                         level = driver.find_elements_by_class_name("monster-container")[0].find_elements_by_tag_name("p")[0]
                         level = level.text.split(":")[1].split("\n")[0][1:]
                         decision = "n"
@@ -123,27 +115,62 @@ def training(driver, url, leaders, iteration):
                             flag = True
                         else:
                             print("Parabéns, você atingiu o level 100!")
-                        time.sleep(2)
                     if decision == "s":
                         # rebattle opponent
-                        nextPage = driver.find_elements_by_class_name("menu-tab")[0].find_element_by_tag_name("a")
-                        nextPage.click()
-                        time.sleep(2)
+                        onClick(driver.find_elements_by_class_name("menu-tab")[0].find_element_by_tag_name("a"), 0, sleep)
                         flag = True
                     continue           
 
-def selectAttack(driver, url):
-    driver.find_element_by_id("pokeName1").click()
-    time.sleep(2)
+def selectAttack(driver, url, sleep):
+    # permite escolher o ataque do pokemon
+    onClick(driver.find_element_by_id("pokeName1"), 0, sleep)
     col = driver.find_elements_by_class_name("valign-top")[-1].find_elements_by_tag_name("td")[0]
     col = col.text.split("\n")
     for i in range(1, len(col)):
         print(str(i)+": "+col[i])
     choice = input("Escolha o attack: ")
-    driver.find_element_by_id("pokedexTab").click()
-    time.sleep(2)
-
+    onClick(driver.find_element_by_id("pokedexTab"), 0, sleep)
+    
     return int(choice)
+
+def battleRound(driver, element, att, first, low, sleep):
+    # round de batalha
+    value = element.get_attribute("value")
+    if "select_attack" in value:
+        print("Continue")
+    elif "attack" in value:   
+        if first:
+            print("Choice attack!")
+            changeAttack(driver, att, sleep)
+            first = False
+        else:
+            usePotion(driver, low, sleep)
+        print("Attack")
+    onClick(element, 1, sleep)
+
+    return first
+
+def usePotion(driver, low, sleep):
+    # usar pot quando vida menor que low
+    hp = driver.find_elements_by_tag_name("strong")[1].text.split(": ")[-1]
+    if int(hp) <= low:
+        onClick(driver.find_element_by_id("item1"), 0, sleep)
+        bts = driver.finds_element_by_class_name("button-small")
+        for bt in bts:
+            if "Use Item" in bt.get_attribute("value"):
+                print("HP: "+hp+". Use potion!")
+                onClick(bt, 1, sleep)
+                break
+            
+def changeAttack(driver, att, sleep):
+     onClick(driver.find_element_by_class_name("attackForm").find_elements_by_tag_name("td")[att-1].find_element_by_tag_name("label"), 0, sleep)
+
+def onClick(element, click, sleep):
+    if click == 0:
+        element.click()
+    elif click == 1:
+        element.submit()
+    time.sleep(sleep)
 
 if __name__ == "__main__":
     main()
